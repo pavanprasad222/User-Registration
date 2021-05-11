65d8f54b70a013cef5add15a2ac94e5c5bc68631
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-12 00:17:28 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -7,24 +7,22 @@ package userregistration;
 	    private boolean check;
 	    private String password;
 	    private static final Scanner SCANNER  = new Scanner(System.in);
-
-	    /**
-	     * Password must contain of 8 characters.
-	     * Have a Special Character(eg-@$^)"
-	     */
-	    public void checkPassword() {
-	        System.out.println("Enter Password(min 8 characters): ");
-	        System.out.println("*Have a Special Character(Eg-@$^)");
-	        password = SCANNER.nextLine();
-	        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z]){8,}.*$").matcher(password).matches();
-	        if (!check) {
-	            System.out.println(" Invalid Password!! Enter a valid one. ");
-	            checkPassword();
-	        }
-	        else {
-	            System.out.println("Done! Password is valid ");
-	        }
-	    }
+		
+		/**
+     * Password must be of 8 characters.
+     */
+    public void checkPassword() {
+        System.out.println("Enter Password(min 8 character): ");
+        password = SCANNER.nextLine();
+        check = Pattern.compile("^[a-z]{8,}$").matcher(password).matches();
+        if (!check) {
+            System.out.println(" Invalid Password!! Enter a valid one. ");
+            checkPassword();
+        }
+        else {
+            System.out.println(" Password is valid ");
+        }
+    }
 	   
 	
 	 public static void main(String[]args) {
