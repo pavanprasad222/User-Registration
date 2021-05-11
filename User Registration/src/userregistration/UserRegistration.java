a0b9a0c7bb3f3a085f8795dd9d94eb312bf5b608
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-11 10:49:32 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -5,28 +5,28 @@ package userregistration;
 	
 	public class UserRegistration {
 	    private boolean check;
-	    private String phoneNumber;
+	    private String Email;
 	    private static final Scanner SCANNER  = new Scanner(System.in);
 
 
-	    /**
-	     * check mobile number
-	     */
-	    public void checkPhoneNumber() {
-	        System.out.println("Enter Your phoneNumber (Eg. +91 8074222312) : ");
-	        phoneNumber = SCANNER.nextLine();
-	        check = Pattern.compile("^[\0-9]{1,3} [0-9]{10}$").matcher(phoneNumber).matches();
-	        if (!check) {
-	            System.out.println(" Number is invalid! !Please Enter a Valid Number. ");
-	            checkPhoneNumber();
-	        }
-	        else {
-	            System.out.println("Done ! Valid Number ");
-	        }
-	    }
+	 /**
+     * Added checkEmail method to match the email ids according to the given pattern.
+     */
+    public void checkEmail() {
+        System.out.println("Enter a valid email(Eg. abc.xyz@bl.co.in) : ");
+        email = SCANNER.nextLine();
+        check = Pattern.compile("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$").matcher(email).matches();
+        if (!check) {
+            System.out.println(" Invalid!!Email-id...Please Enter a Valid one. ");
+            checkEmail();
+        }
+        else {
+            System.out.println(" Email-Id is valid ");
+        }
+    }
 	
 	 public static void main(String[]args) {
 	        UserRegistration user = new UserRegistration();
-	        user.checkPhoneNumber();
+	        user.checkEmail();
 	 }
 }
