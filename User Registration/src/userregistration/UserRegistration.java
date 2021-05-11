ff2dc4833e3cfed4ebfb43a35de160bce4b07f50
pavan sriram prasad <pittapavansriramprasad@gmailcom>
2021-05-12 00:29:58 +0530

    password with min 8 characters and one upper case also numerical

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -8,21 +8,24 @@ package userregistration;
 	    private String password;
 	    private static final Scanner SCANNER  = new Scanner(System.in);
 
+	   
 	    /**
-	     * Password must contain of 8 characters.
-	     * Have a Special Character(eg-@$^)"
+	     * Password must be of 8 characters.
+	     * Have a Special Character(Eg-@$^)"
+	     * Must Have a Number.
 	     */
 	    public void checkPassword() {
 	        System.out.println("Enter Password(min 8 characters): ");
 	        System.out.println("*Have a Special Character(Eg-@$^)");
+	        System.out.println("*Have a Number(0-9)");
 	        password = SCANNER.nextLine();
-	        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z]){8,}.*$").matcher(password).matches();
+	        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]){8,}.*$").matcher(password).matches();
 	        if (!check) {
 	            System.out.println(" Invalid Password!! Enter a valid one. ");
 	            checkPassword();
 	        }
 	        else {
-	            System.out.println("Done! Password is valid ");
+	            System.out.println(" Password is valid ");
 	        }
 	    }
 	   
