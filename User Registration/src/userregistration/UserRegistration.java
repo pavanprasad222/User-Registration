5d75f64217a43b24be71b96ea875ae342c63ff27
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-12 23:40:41 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -10,9 +10,7 @@ package userregistration;
 
 	   
 	    public void checkPassword() {
-	        System.out.println("Enter Password(min 8 characters): ");
-	        System.out.println("*Have a Special Character(Eg-@$^)");
-	        System.out.println("*Have a Number(0-9)");
+	        System.out.println("Enter Password(min 8 characters, Have a Special Character, Have a Numerical): ");
 	        password = SCANNER.nextLine();
 	        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]){8,}.*$").matcher(password).matches();
 	        if (!check) {
