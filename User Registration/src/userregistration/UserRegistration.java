0a68cf11e7e6c90e6a4b73d17c17dc91ee74093d
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-12 23:19:37 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -7,16 +7,13 @@ package userregistration;
 	    private boolean check;
 	    private String firstName,lastName,email,phoneNumber,password;
 	    private static final Scanner SCANNER  = new Scanner(System.in);
-	/**
-	 *checking first name according to pattern.
-	 * for this operation i have imported regex module and Scanner module.
-	 */
+	
 	    public void checkFirstName() {
 	        System.out.println("Enter First-name starts with capital letter and has minimum 3 characters : ");
 	        firstName = SCANNER.nextLine();
 	        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
 	        if (!check) {
-	            System.out.println("Invalid! !Please Enter your valid firstname .");
+	            System.out.println("Sorry! Wrong Input .");
 	            checkFirstName();
 	        }
 	        else {
@@ -24,46 +21,38 @@ package userregistration;
 	        }
 	    }
 
-	    /**
-	     * added check last name method according to pattern.
-	     */
+	 
 	    public void checkLastName() {
 	        System.out.println("Enter Last-name starts with capital letter and has minimum 3 characters : ");
 	        lastName = SCANNER.nextLine();
 	        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
 	        if (!check) {
-	            System.out.println("Invalid!!Please Enter your name valic lastname.");
+	            System.out.println("Sorry! Wrong Input");
 	            checkLastName();
 	        }
 	        else
 	            System.out.println(" Done !It's  valid Lastname ");
 	    }
 
-	    /**
-	     * checkEmail method to match the email Id'S according to the pattern.
-	     */
+	   
 	    public void checkEmail() {
 	        System.out.println("Enter a valid email(Eg. abc.psr@bl.co.in) : ");
 	        email = SCANNER.nextLine();
 	        check = Pattern.compile("^[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(?:\\.[a-zA-Z0-9]{2,}+)*$").matcher(email).matches();
 	        if (!check) {
-	            System.out.println(" Invalid!!Email-ID ! please Enter a Valid input. ");
+	            System.out.println("Sorry ! Wrong Input ");
 	            checkEmail();
 	        }
 	        else {
 	            System.out.println(" Email-Id is valid ");
-	        }
-	    }
-
-	    /**
-	     * This checkPhonenumber method will check the user mobile number according to the pattern.
-	     */
+		}
+		    
 	    public void checkPhoneNumber() {
 	        System.out.println("Enter Your Mobile Number (Eg. +91 9919819801) : ");
 	        phoneNumber = SCANNER.nextLine();
 	        check = Pattern.compile("^[\0-9]{1,3} [0-9]{10}$").matcher(phoneNumber).matches();
 	        if (!check) {
-	            System.out.println(" Number is invalid! !Please Enter a Valid Number. ");
+	            System.out.println(" Sorry! Wrong Input ");
 	            checkPhoneNumber();
 	        }
 	        else {
