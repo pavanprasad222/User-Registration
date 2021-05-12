c07b876d697fc994862260bf6d5f111ce7faf83c
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-12 23:05:42 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -9,15 +9,12 @@ package userregistration;
 	    private static final Scanner SCANNER  = new Scanner(System.in);
 
 
-	    /**
-	     * added check last name method according to pattern.
-	     */
 	    public void checkLastName() {
 	        System.out.println("Enter Last-name starts with capital letter and has minimum 3 characters : ");
 	        lastName = SCANNER.nextLine();
 	        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(lastName).matches();
 	        if (!check) {
-	            System.out.println("Invalid!!Please Enter your name valic lastname.");
+	            System.out.println("Sorry! wrong input.");
 	            checkLastName();
 	        }
 	        else
@@ -26,7 +23,10 @@ package userregistration;
 	
 	
 	 public static void main(String[]args) {
+		 
 	        UserRegistration user = new UserRegistration();
+		 
 	        user.checkLastName();
+		 
 	 }
 }
