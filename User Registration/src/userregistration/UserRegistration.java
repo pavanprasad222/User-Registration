f8cac46a47702abda90f5d1dd005404dc1629332
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-12 23:09:28 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -9,15 +9,13 @@ package userregistration;
 	    private static final Scanner SCANNER  = new Scanner(System.in);
 
 
-	    /**
-	     * check mobile number
-	     */
+	  
 	    public void checkPhoneNumber() {
 	        System.out.println("Enter Your phoneNumber (Eg. +91 9919819801) : ");
 	        phoneNumber = SCANNER.nextLine();
 	        check = Pattern.compile("^[\0-9]{1,3} [0-9]{10}$").matcher(phoneNumber).matches();
 	        if (!check) {
-	            System.out.println(" Number is invalid! !Please Enter a Valid Number. ");
+	            System.out.println(" Sorry! Wrong Input ");
 	            checkPhoneNumber();
 	        }
 	        else {
@@ -26,7 +24,9 @@ package userregistration;
 	    }
 	
 	 public static void main(String[]args) {
+		 
 	        UserRegistration user = new UserRegistration();
+		 
 	        user.checkPhoneNumber();
 	 }
 }
