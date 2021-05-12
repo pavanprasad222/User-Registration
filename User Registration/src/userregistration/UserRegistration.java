dffea9fd2660ba59911b35c999a4dc9348676d16
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-12 23:03:42 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -7,16 +7,13 @@ package userregistration;
 	    private boolean check;
 	    private String firstName;
 	    private static final Scanner SCANNER  = new Scanner(System.in);
-	/**
-	 *checking first name according to pattern.
-	 * for this operation i have imported regex module and Scanner module.
-	 */
+		
 	    public void checkFirstName() {
 	        System.out.println("Enter First-name starts with capital letter and has minimum 3 characters : ");
 	        firstName = SCANNER.nextLine();
 	        check = Pattern.compile("[A-Z]{1}[a-z]{2,}").matcher(firstName).matches();
 	        if (!check) {
-	            System.out.println("Invalid! !Please Enter your valid firstname .");
+	            System.out.println("Sorry! Wrong Input .");
 	            checkFirstName();
 	        }
 	        else {
@@ -25,9 +22,10 @@ package userregistration;
 	    }
 
     public static void main(String[]args) {
+	    
 	            UserRegistration user = new UserRegistration();
-	            user.checkFirstName();
-	     
+	    
+	            user.checkFirstName();    
 	    }
 	}
 
