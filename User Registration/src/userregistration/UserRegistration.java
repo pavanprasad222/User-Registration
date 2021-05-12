8e3f7c20248cc04edf422ca12de6c5062b8ab402
pavanprasad222 <82253376+pavanprasad222@users.noreply.github.com>
2021-05-12 23:34:29 +0530

    Update UserRegistration.java

--- User Registration/src/userregistration/UserRegistration.java
+++ User Registration/src/userregistration/UserRegistration.java
@@ -20,6 +20,21 @@ package userregistration;
 	            System.out.println(" Done ! It's  Valid Firstname ");
 	        }
 	    }
+		
+		public void checkPassword() {
+	        System.out.println("Enter Password(min 8 characters): ");
+	        System.out.println("*Have a Special Character(Eg-@$^)");
+	        System.out.println("*Have a Number(0-9)");
+	        password = SCANNER.nextLine();
+	        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]){8,}.*$").matcher(password).matches();
+	        if (!check) {
+	            System.out.println(" Sorry! Wrong Input ");
+	            checkPassword();
+	        }
+	        else {
+	            System.out.println(" Password is valid ");
+	        }
+	    }
 
 	 
 	    public void checkLastName() {
@@ -86,6 +101,20 @@ package userregistration;
 	        }
 	        else {
 	            System.out.println("Done! Password is valid ");
+	        }
+	    }
+public void checkPassword() {
+	        System.out.println("Enter Password(min 8 characters): ");
+	        System.out.println("*Have a Special Character(Eg-@$^)");
+	        System.out.println("*Have a Number(0-9)");
+	        password = SCANNER.nextLine();
+	        check = Pattern.compile("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]){8,}.*$").matcher(password).matches();
+	        if (!check) {
+	            System.out.println(" Sorry! Wrong Input ");
+	            checkPassword();
+	        }
+	        else {
+	            System.out.println(" Password is valid ");
 	        }
 	    }
 
